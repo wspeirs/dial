ef15902d0c15b68b4f0a82c2acb4c432fb200309
William Speirs <bill.speirs@gmail.com>
2025-04-17 22:19:11 -0400

    Removed some left-recurions and started testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use pest::Parser;
+use pest_derive::Parser;
+
+
+// #[cfg(debug_assertions)]
+// const _GRAMMAR: &'static str = include_str!("grammar.pest"); // relative to this file
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"] // relative to src
+struct DialParser;
+
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("test/gemini_generated.dial");
+
+    println!("Test file: {}", path.canonicalize().unwrap().display());
+
+    let mut file = File::open(path).unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    // parse the file
+    let _pairs = DialParser::parse(Rule::file, &contents).unwrap();
 }
